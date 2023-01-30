b0e984a7cf66e1f3c9e225e26204ce6f299b12df
Rodrigo Amarilla <83515418+RodriAmarilla@users.noreply.github.com>
2023-01-29 21:21:57 -0300

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,12 @@
+document.getElementById("boton").onclick = function () {
+    console.log("Evento click capturado");
+    document.getElementById("oculto").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem fugiat sunt corporis. Minus ullam quo, voluptatum praesentium tempore vero natus hic suscipit corporis quae veritatis! Magni repellendus enim repudiandae nisi.";
+    document.getElementById("ctitulo_1").style.color = "salmon";
+    document.getElementById("ctitulo_2").style.color = "salmon";
+    document.getElementById("ctitulo_3").style.color = "salmon";
+    document.getElementById("ctitulo_4").style.color = "salmon";
+    document.getElementById("ctitulo_5").style.color = "salmon";
+    document.getElementById("ctitulo_6").style.color = "salmon";
+};
+
+
